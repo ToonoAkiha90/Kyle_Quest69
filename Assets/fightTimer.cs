3ceebbee7e024bc2c1febf4b1c838ac65daa52aa
ToonoAkiha90 <52953800+ToonoAkiha90@users.noreply.github.com>
2019-08-02 22:25:03 -0700

    wait timer up

---
+++ Assets/fightTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fightTimer : MonoBehaviour
+{
+    public GameObject downCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("StartDelay");
+    }
+
+    IEnumerator StartDelay ()
+    {
+        Time.timeScale = 0;
+        float pauseTime = Time.realtimeSinceStartup + 3f;
+        while (Time.realtimeSinceStartup < pauseTime)
+            yield return 0;
+        downCount.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
