1c05a3c8622029ba6606a4d1a2d486936eacf2b2
ToonoAkiha90 <52953800+ToonoAkiha90@users.noreply.github.com>
2019-08-09 08:11:56 -0700

    Boss Follows!

---
+++ Assets/Scripts/Boss/BossMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private Transform player;
+    private float dodge;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        //Chasing if's
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            //-speed does the retreating
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+    }
+}
