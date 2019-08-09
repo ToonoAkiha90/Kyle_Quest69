f99148f164ebbdb9095ed46933d77ab7882a688a
ToonoAkiha90 <52953800+ToonoAkiha90@users.noreply.github.com>
2019-08-08 23:39:10 -0700

    Screenbounds by cameraView

--- Assets/Scripts/Boundary.cs
+++ Assets/Scripts/Boundary.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Boundary : MonoBehaviour
 {
-    private Vector2 boundary;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -7.5f, 7.5f),
-            Mathf.Clamp(transform.position.y, -4f, 1f), transform.position.z);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        //bound.size is divided by 2 because screenbounds are relative to the sprite's center
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 objectPosition = transform.position;
+        //screenbound position goes left to right (-screenbounds, screenbounds)
+        objectPosition.x = Mathf.Clamp(objectPosition.x, -screenBounds.x + objectWidth, screenBounds.x - objectWidth);
+        objectPosition.y = Mathf.Clamp(objectPosition.y, -screenBounds.y + objectHeight, screenBounds.y - objectHeight);
+        transform.position = objectPosition;
     }
 }
